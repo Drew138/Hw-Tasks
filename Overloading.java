73568fdf8a7b24acf61e6a37fc3c257499ad9c06
drew <andresalazar138@gmail.com>
2020-09-16 12:03:33 -0500

    finished Overloading class

--- Overloading.java
+++ Overloading.java
@@ -1,3 +1,17 @@
 public class Overloading {
+    public void mostrar(int i) {
+        System.out.println("Se ha ingresado int");
+    }
 
+    public void mostrar(double i) {
+        System.out.println("Se ha ingresado double");
+    }
+
+    public static void mostrar(String i) {
+        System.out.println("Se ha ingresado String");
+    }
+
+    public static void mostrar(char i) {
+        System.out.println("Se ha ingresado char");
+    }
 }
